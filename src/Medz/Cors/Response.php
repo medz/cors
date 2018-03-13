febe6bacee66f60b146a0405a0e454d18ad16098
Seven Du <shiweidu@outlook.com>
2018-03-13 16:16:32 +0800

    chore: Add response hepers.

--- src/Medz/Cors/Response.php
+++ src/Medz/Cors/Response.php
@@ -28,6 +28,64 @@ class Response
         }
 
         $this->response[$name] = $value;
+
+        return $this;
+    }
+
+    public function setAccessControlAllowCredentials(bool $credentiale)
+    {
+        $this->setHeader('Access-Control-Allow-Credentials', $credentiale ? 'true', 'false');
+
+        return $this;
+    }
+
+    public function setAccessControlAllowMethods($methods)
+    {
+        if (is_array($methods)) {
+            $methods = implode(', ', $methods);
+        }
+
+        $this->setHeader('Access-Control-Allow-Methods', (string) $methods);
+
+        return $this;
+    }
+
+    public function setAccessControlAllowHeaders($headers)
+    {
+        if (is_array($headers)) {
+            $headers = implode(', ', $headers);
+        }
+
+        $this->setHeader('Access-Control-Allow-Headers', (string) $headers)
+
+        return $this;
+    }
+
+    public function setAccessControlAllowOrigin(string $origin)
+    {
+        $this->setHeader('Access-Control-Allow-Origin', $origin);
+
+        return $this;
+    }
+
+    public function setAccessControlExposeHeaders($headers)
+    {
+        if (is_array($headers)) {
+            $headers = implode(', ', $headers);
+        }
+
+        $this->setHeader('Access-Control-Expose-Headers', (string) $headers);
+
+        return $this;
+    }
+
+    public function setAccessControlMaxAge(int $maxAge = 0)
+    {
+        if ($maxAge) {
+            $this->setHeader('Access-Control-Max-Age', $maxAge);
+        }
+
+        return $this;
     }
 
     public function returnNative()
