78042475d2f996d558c98ec12949f00e2d1be50f
Seven Du <shiweidu@outlook.com>
2018-03-13 08:43:56 +0000

    Apply fixes from StyleCI

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'allow-headers' => ['*'], // ex: Content-Type, Accept
+    'allow-headers'  => ['*'], // ex: Content-Type, Accept
     'expose-headers' => [],
-    'origins' => ['*'], // ex: http://localhost
-    'methods' => ['*'], // ex: GET, POST, PUT, PATCH, DELETE
-    'max-age' => 0,
+    'origins'        => ['*'], // ex: http://localhost
+    'methods'        => ['*'], // ex: GET, POST, PUT, PATCH, DELETE
+    'max-age'        => 0,
 ];
