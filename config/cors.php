d1b8c281efba856ba14f40d714515b6754981af0
Seven Du <shiweidu@outlook.com>
2018-03-13 16:43:48 +0800

    chore: Add example conifg file.

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'allow-headers' => ['*'], // ex: Content-Type, Accept
+    'expose-headers' => [],
+    'origins' => ['*'], // ex: http://localhost
+    'methods' => ['*'], // ex: GET, POST, PUT, PATCH, DELETE
+    'max-age' => 0,
+];
