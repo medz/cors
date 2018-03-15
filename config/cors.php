a48c3c8b4f94b877bc915c71e168376229dd1a04
Seven Du <shiweidu@outlook.com>
2018-03-15 07:49:35 +0000

    Apply fixes from StyleCI

--- config/cors.php
+++ config/cors.php
@@ -2,12 +2,12 @@
 
 return [
     'allow-credentiails' => false, // set "Access-Control-Allow-Credentials" 👉 string "false" or "true".
-    'allow-headers'  => ['*'], // ex: Content-Type, Accept, X-Requested-With
-    'expose-headers' => [],
-    'origins'        => ['*'], // ex: http://localhost
-    'methods'        => ['*'], // ex: GET, POST, PUT, PATCH, DELETE
-    'max-age'        => 0,
-    'laravel'        => [
+    'allow-headers'      => ['*'], // ex: Content-Type, Accept, X-Requested-With
+    'expose-headers'     => [],
+    'origins'            => ['*'], // ex: http://localhost
+    'methods'            => ['*'], // ex: GET, POST, PUT, PATCH, DELETE
+    'max-age'            => 0,
+    'laravel'            => [
         'prepend-global-middleware' => true,
         'using-http-message-type'   => 'symfony', // Laravel using `symfong/http-foundation` package, Default using symfony, If you use PSR-7 in Laravel, set `using-http-message-type` to `psr-7`.
         'allow-route-perfix'        => '',
