9175da8e6324d36f6d25e6c88b81c0057ec3e3c2
Seven Du <shiweidu@outlook.com>
2018-04-24 17:11:52 +0800

    feat(PRS-15): support prs-15

---
+++ src/PSR/CorsMiddleware.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Medz\Cors\PSR;
+
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Medz\Cors\CorsInterface;
+
+class CorsMiddleware implements MiddlewareInterface
+{
+    /**
+     * The CORS instance.
+     *
+     * @var \Medz\Cors\CorsInterface
+     */
+    protected $cors;
+
+    /**
+     * Create the middleware.
+     *
+     * @param \Medz\Cors\CorsInterface $cors
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function __construct(CorsInterface $cors)
+    {
+        $this->cors = $cors;
+    }
+
+    /**
+     * The middleware process.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Server\RequestHandlerInterface $handle
+     * @return \Psr\Http\Message\ResponseInterface
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handle): ResponseInterface
+    {
+        $type = 'psr-7';
+        if ($this->cors->isPreflightRequest($type, $request)) {
+            $this->cors->setRequest($type, $request);
+            $this->cors->setResponse('array', []);
+            $this->cors->handle();
+
+            ob_end_clean();
+            ob_start();
+            $headers = $this->cors->getResponse();
+            $statusCode = 204;
+            foreach ($headers as $headerKey => $headerValue) {
+                header($headerKey.': '.$headerValue, false, $statusCode);
+            }
+            header('HTTP/1.1 204 No Content');
+            ob_end_flush();
+            exit;
+        }
+
+        return $handle->handle($request);
+    }
+}
