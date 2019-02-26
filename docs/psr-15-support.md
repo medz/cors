d6fbcdde2799ccaa3eeedabd1c96207c21aea5e7
Seven Du <shiweidu@outlook.com>
2019-02-26 15:16:41 +0800

    refactor(PSR): Refactor PSR-15 middleware

--- docs/psr-15-support.md
+++ docs/psr-15-support.md
@@ -8,15 +8,27 @@ Using this middleware depends on your preference or where you are using PSR-15.
 
 Just like PSR-7 is used in the Slim framework! Based on your specific code pattern for middleware use, you can refer to the use of the Slim framework.
 
-**See**👉 [Using by Slim](https://github.com/medz/cors/blob/master/docs/using-by-slim.md)
-
 ## Example
 
 `Medz\Cors\PSR\CorsMiddleware` :
 ```php
-$settings = []; // The config.
-$cors = new Medz\Cors\Cors($settings); // Create CORS instance.
-$middleware = new Medz\Cors\PSR\CorsMiddleware($cors); // Create CORS middleware instance
+
+// Settings.
+$settings = [
+    'allow-credentials'  => false,
+    'allow-headers'      => ['*'],
+    'expose-headers'     => [],
+    'origins'            => ['*'],
+    'methods'            => ['*'],
+    'max-age'            => 0,
+];
+
+// $cors = new Medz\Cors\Cors($settings); // Create CORS instance.
+
+// Create CORS middleware instance
+$middleware = new Medz\Cors\PSR\CorsMiddleware($settings /* $cors */);
 
 // TODO.
 ```
+
+**See**👉 [Using by Slim](https://github.com/medz/cors/blob/master/docs/using-by-slim.md)
--- src/PSR/CorsMiddleware.php
+++ src/PSR/CorsMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace Medz\Cors\PSR;
 
+use Medz\Cors\Cors;
 use Medz\Cors\CorsInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -18,47 +19,100 @@ class CorsMiddleware implements MiddlewareInterface
     protected $cors;
 
     /**
+     * Only using preflight request.
+     * 
+     * @var bool
+     */
+    protected $onlyPreflight;
+
+    /**
      * Create the middleware.
      *
-     * @param \Medz\Cors\CorsInterface $cors
+     * @param \Medz\Cors\CorsInterface|array $payload
      *
      * @author Seven Du <shiweidu@outlook.com>
      */
-    public function __construct(CorsInterface $cors)
-    {
-        $this->cors = $cors;
+    public function __construct($payload = null, $onlyPreflight = false)
+    {   
+        $this->cors = static::resoveCors($payload);
+        $this->onlyPreflight = $onlyPreflight;
     }
 
     /**
      * The middleware process.
      *
      * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Server\RequestHandlerInterface $handle
+     * @param \Psr\Http\Server\RequestHandlerInterface $handler
      *
      * @return \Psr\Http\Message\ResponseInterface
      *
      * @author Seven Du <shiweidu@outlook.com>
      */
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handle): ResponseInterface
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $type = 'psr-7';
-        if ($this->cors->isPreflightRequest($type, $request)) {
-            $this->cors->setRequest($type, $request);
-            $this->cors->setResponse('array', []);
-            $this->cors->handle();
-
-            ob_end_clean();
-            ob_start();
-            $headers = $this->cors->getResponse();
-            $statusCode = 204;
-            foreach ($headers as $headerKey => $headerValue) {
-                header($headerKey.': '.$headerValue, false, $statusCode);
+        $response = $handler->handle($request);
+        if ($this->onlyPreflight) {
+            if ($this->cors->isPreflightRequest('psr-7', $request)) {
+                return $this
+                    ->handle($request, $response)
+                    ->withStatus(204)
+                ;
             }
-            header('HTTP/1.1 204 No Content');
-            ob_end_flush();
-            exit;
+
+            return $response;
         }
 
-        return $handle->handle($request);
+        return $this->handle($request, $response);
+    }
+
+    /**
+     * Process cors headers.
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $type = 'psr-7';
+        $this->cors->setRequest($type, $request);
+        $this->cors->setResponse($type, $response);
+        $this->cors->handle();
+
+        return $this->cors->getResponse();
+    }
+
+    /**
+     * Resolve cors instance.
+     * @param \Medz\Cors\CorsInterface|array $payload
+     * @return \Medz\Cors\CorsInterface
+     */
+    protected function resoveCors($payload): CorsInterface
+    {
+        if ($payload instanceof CorsInterface) {
+            return $payload;
+        } elseif (! is_array($payload)) {
+            $payload = [];
+        }
+
+        return new Cors(static::parseSettings($payload));
+    }
+
+    /**
+     * Parse settings.
+     * @param array $settings
+     * @return array
+     */
+    protected function parseSettings(array $settings): array
+    {
+        $defaultSettings = [
+            'allow-credentials'  => false,
+            'allow-headers'      => ['*'],
+            'expose-headers'     => [],
+            'origins'            => ['*'],
+            'methods'            => ['*'],
+            'max-age'            => 0,
+        ];
+
+        return array_merge($defaultSettings, $settings);
     }
 }
